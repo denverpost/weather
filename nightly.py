862c88e37185d26c0e33169b697c45ce27a544ec
freejoe76 <jmurphy@denverpost.com>
2015-04-20 18:20:58 -0600

    Start writing the nightly weather job

---
+++ nightly.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Write our nightly weather files
+import doctest
+from datetime import date, timedelta
+import argparse
+from accuweather import WeatherData, PublishWeather
+
+def main(args):
+    wd = WeatherData(args)
+    if args:
+        for arg in args.locations[0]:
+            if args.verbose:
+                print arg
+            wd.set_location_key(arg)
+
+
+
+if __name__ == '__main__':
+    """ Takes a list of locations, passed as args.
+        Example:
+        $ python accuweather.py Denver Aspen "Grand Junction"
+        """
+    parser = argparse.ArgumentParser(usage='', description='',
+                                     epilog='')
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
+    parser.add_argument("-c", "--cache", dest="cache", default=False, action="store_true")
+    parser.add_argument("locations", action="append", nargs="*")
+    args = parser.parse_args()
+    print args
+
+    if args.verbose:
+        doctest.testmod(verbose=args.verbose)
+
+    main(args)
