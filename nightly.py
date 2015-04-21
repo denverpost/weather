05ecb9a69f8ee0bcf14d0e279972e2818702f15d
freejoe76 <jmurphy@denverpost.com>
2015-04-21 12:12:01 -0600

    Write the help text

--- nightly.py
+++ nightly.py
@@ -7,23 +7,30 @@ from accuweather import WeatherData, PublishWeather
 
 def main(args):
     wd = WeatherData(args)
+    content = []
     if args:
         for arg in args.locations[0]:
             if args.verbose:
                 print arg
             wd.set_location_key(arg)
+            request = { 
+                'type': 'currentconditions',
+                'slug': '',
+                'suffix': '&details=true'
+            }
+            wd.get_from_api(arg, **request)
+            wd.write_cache(arg, **request)
+            print wd.respose
 
 
 
 if __name__ == '__main__':
-    """ Takes a list of locations, passed as args.
-        Example:
-        $ python nightly.py Denver Aspen "Grand Junction"
-        """
-    parser = argparse.ArgumentParser(usage='', description='',
+    parser = argparse.ArgumentParser(usage='$ python nightly.py Denver Aspen "Grand Junction"',
+                                     description='Takes a list of locations passed as args.',
                                      epilog='')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
-    parser.add_argument("-c", "--cache", dest="cache", default=False, action="store_true")
+    parser.add_argument("-c", "--cache", dest="cache", default=False, action="store_true",
+                        help="Pull from local cached data, make no external calls to Accuweather's API")
     parser.add_argument("locations", action="append", nargs="*")
     args = parser.parse_args()
 
