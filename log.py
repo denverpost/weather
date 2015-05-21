dd0fd38337ba062c57b00555323ada1e040ca452
freejoe76 <jmurphy@denverpost.com>
2015-05-21 18:07:53 -0500

    Logging almost ready to launch

--- log.py
+++ log.py
@@ -3,12 +3,22 @@
 # Log the days we've written our nightly weather files.
 import doctest
 import argparse
+from datetime import date
 
 def main(args):
+    path_vars = {
+        'year': date.today().year,
+        'month': date.strftime(date.today(), '%B').lower(),
+        'day': date.today().day
+    }
+    url = '%(year)s/%(month)s/%(day)s' % path_vars
+    day = date.strftime(date.today(), '%Y,%B,%-m')
+    record = "%s,%s" % (day,url)
+
     # Write the log file. 
-    f = open('log_daily', 'wb')
+    f = open('log_daily.csv', 'wb')
     f.write(output)
-    #f.close()
+    f.close()
 
 if __name__ == '__main__':
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
