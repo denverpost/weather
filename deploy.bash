cef9d0be7b9b12a55f3b07250e0b8e91f1ae1283
freejoe76 <jmurphy@denverpost.com>
2015-06-03 13:30:29 -0600

    Add output of the day's weather headlines to the deploy script

--- deploy.bash
+++ deploy.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Update the weather data.
 source source.bash
+DATE_SLUG=`date +'%F'`
 NIGHTLY=0
 while [ "$1" != "" ]; do
     case $1 in
@@ -15,6 +16,7 @@ if [ "$NIGHTLY" -eq 0 ]; then
     for CITY in `cat colorado-cities.txt`; do echo $CITY; python accuweather.py $CITY; done
 else
     for CITY in `cat colorado-cities.txt`; do echo $CITY; python nightly.py $CITY; done
+    python recentfeed.py http://rss.denverpost.com/mngi/rss/CustomRssServlet/36/213601.xml > "www/output/headlines_"$DATE_SLUG".html"
     RECORD=`python log.py`
     # Also build the indexes
     python nightly.py --indexes # This runs all the indexes
