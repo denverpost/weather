7258fba002b11b98a2a5a8e515217bdc369b5291
freejoe76 <jmurphy@denverpost.com>
2016-10-06 12:14:50 -0500

    Update feed URL

--- deploy.bash
+++ deploy.bash
@@ -15,7 +15,7 @@ done
 if [ "$NIGHTLY" -eq 0 ]; then
     for CITY in `cat colorado-cities.txt`; do echo $CITY; python accuweather.py $CITY; done
 else
-    python recentfeed.py http://rss.denverpost.com/mngi/rss/CustomRssServlet/36/213601.xml > "www/output/headlines_"$DATE_SLUG".html"
+    python recentfeed.py http://www.denverpost.com/news/weather/feed/ > "www/output/headlines_"$DATE_SLUG".html"
     for CITY in `cat colorado-cities.txt`; do echo $CITY; python nightly.py $CITY; done
     RECORD=`python log.py`
     # Also build the indexes
